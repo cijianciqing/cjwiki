2419e9558a3a321f026e145790c6aeb638369816
cjcq <425430798@qq.com>
2021-12-17 16:58:25 +0800

    2-2 init

---
+++ src/test/java/cj/springboot/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package cj.springboot.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
