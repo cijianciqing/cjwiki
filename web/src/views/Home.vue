864984a169c7d8b57663fe56f90587098e591516
cjcq <425430798@qq.com>
2022-01-26 14:06:36 +0800

    4-5 Ant Design Vue 2.2.8

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,11 +3,17 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
+  <div>
+    <a-button type="primary">
+      Primary
+    </a-button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Button } from 'ant-design-vue';
 
 export default defineComponent({
   name: 'Home',
